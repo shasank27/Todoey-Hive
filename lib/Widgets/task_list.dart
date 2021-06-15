acfe69f234c51f56d5cbbc2917d097eb9e824be1
shasank27 <shasankperiwal2027@gmail.com>
2021-06-15 10:02:46 +0530

    Major UI overhaul

--- lib/Widgets/task_list.dart
+++ lib/Widgets/task_list.dart
@@ -7,6 +7,89 @@ import 'package:todoey_flutter/data/taskchangenotifier.dart';
 import 'package:todoey_flutter/screens/task_screen.dart';
 import 'task_tile.dart';
 
+class TaskListDone extends StatefulWidget {
+  @override
+  _TaskListDoneState createState() => _TaskListDoneState();
+}
+
+class _TaskListDoneState extends State<TaskListDone> {
+  TaskChangeNotifier taskChangeNotifier;
+  final buildtasklist = Hive.box(
+      'todolist'); //.values.where((element) => element.isDone == false);
+  TasksScreen task;
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: Hive.box('todolist').listenable(),
+      builder: (context, value, _) {
+        return ListView.builder(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(top: 10),
+          itemCount: buildtasklist.length,
+          itemBuilder: (context, index) {
+            return TaskTile(
+              isChecked: buildtasklist.getAt(index).isDone,
+              taskTitle: buildtasklist.getAt(index).text,
+              toggleCheckBox: (value) {
+                Provider.of<TaskChangeNotifier>(context, listen: false)
+                    .put(index, value);
+              },
+              deleteonLongPress: () {
+                Alert(
+                  style: AlertStyle(
+                    alertPadding: EdgeInsets.all(10),
+                    alertBorder: RoundedRectangleBorder(
+                      side: BorderSide.none,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(30),
+                      ),
+                    ),
+                    backgroundColor: Theme.of(context).canvasColor,
+                    titleStyle: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    descStyle: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  context: context,
+                  type: AlertType.warning,
+                  title: "Delete Alert",
+                  desc: "Are you sure you want to delete?",
+                  buttons: [
+                    DialogButton(
+                      radius: BorderRadius.circular(20),
+                      child: Text(
+                        "NO",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      onPressed: () => Navigator.pop(context),
+                      color: Colors.blueGrey,
+                    ),
+                    DialogButton(
+                      radius: BorderRadius.circular(20),
+                      child: Text(
+                        "YES",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      onPressed: () {
+                        Provider.of<TaskChangeNotifier>(context, listen: false)
+                            .delete(index);
+                        Navigator.pop(context);
+                      },
+                      color: Colors.pinkAccent,
+                    ),
+                  ],
+                ).show();
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
 class TaskList extends StatefulWidget {
   @override
   _TaskListState createState() => _TaskListState();
@@ -22,6 +105,8 @@ class _TaskListState extends State<TaskList> {
         valueListenable: Hive.box('todolist').listenable(),
         builder: (context, value, _) {
           return ListView.builder(
+            shrinkWrap: true,
+            padding: EdgeInsets.only(top: 10),
             itemCount: buildtasklist.length,
             itemBuilder: (context, index) {
               return TaskTile(
--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -23,6 +23,7 @@ class _TasksScreenState extends State<TasksScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       floatingActionButton: FloatingActionButton(
         child: Icon(
           Icons.add,
@@ -30,7 +31,7 @@ class _TasksScreenState extends State<TasksScreen> {
         ),
         onPressed: () {
           showModalBottomSheet(
-            isScrollControlled: true,
+            isScrollControlled: false,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(30),
@@ -51,49 +52,56 @@ class _TasksScreenState extends State<TasksScreen> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            color: Theme.of(context).backgroundColor,
-            padding: EdgeInsets.only(top: 50, left: 30, right: 30, bottom: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Switch.adaptive(
-                    //activeThumbImage: AssetImage('images/moon.png'),
-                    //inactiveThumbImage: AssetImage('images/sun.png'),
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Theme.of(context).backgroundColor,
+              padding:
+                  EdgeInsets.only(top: 50, left: 30, right: 30, bottom: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Switch(
+                    activeThumbImage: AssetImage('images/moon.png'),
+                    inactiveThumbImage: AssetImage('images/sun.png'),
                     activeTrackColor: Colors.indigo[1000],
                     inactiveTrackColor: Colors.blue[50],
-                    splashRadius: 50,
                     value: Provider.of<ThemeProvider>(context).isDarkMode,
                     onChanged: (value) {
                       final provider =
                           Provider.of<ThemeProvider>(context, listen: false);
                       provider.toggleTheme(value);
-                    }),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Todoey',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 50.0,
-                    fontWeight: FontWeight.w700,
+                    },
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  taskremain(),
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
+                  SizedBox(
+                    height: 10,
                   ),
-                )
-              ],
+                  FittedBox(
+                    child: Text(
+                      'Todoey',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    taskremain(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           Expanded(
+            flex: 7,
             child: Container(
               padding: EdgeInsets.only(left: 30, right: 30),
               decoration: BoxDecoration(
@@ -103,7 +111,24 @@ class _TasksScreenState extends State<TasksScreen> {
                   topRight: Radius.circular(30),
                 ),
               ),
-              child: TaskList(),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 7,
+                    ),
+                    TaskList(),
+                    Divider(
+                      indent: 12,
+                      endIndent: 12,
+                      color: Colors.black,
+                    ),
+                    TaskListDone(),
+                  ],
+                ),
+              ),
             ),
           ),
         ],
